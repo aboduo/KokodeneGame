c5cc388883a574bc4f325bcbf7819b008da5ef3d
Yuuichiro KATO <yonesuke777@gmail.com>
2014-04-14 01:21:57 +0900

    スコア表示

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -2,6 +2,9 @@
 
 USING_NS_CC;
 
+const float POSITION_Y_SPAWN = 800; // ここでねマンが初期表示されるY座標
+const int COUNT_MAN = 5; // ここでねマンの初期残機
+
 Scene* GameScene::createScene()
 {
     auto scene = Scene::createWithPhysics();
@@ -16,6 +19,9 @@ bool GameScene::init()
     {
         return false;
     }
+    
+    _score = 0;
+    _last = COUNT_MAN;
     
     Size visibleSize = Director::getInstance()->getVisibleSize();
 
@@ -45,35 +51,80 @@ bool GameScene::init()
     nami->setPosition(visibleSize.width / 2, 100);
     this->addChild(nami, 1);
 
+    _scoreLabel = Label::createWithBMFont("hase_toppo.fnt", "");
+    _scoreLabel->setAnchorPoint(Point(1, 0.5));
+    _scoreLabel->setScale(0.7);
+    _scoreLabel->setPosition(visibleSize.width - 10, visibleSize.height - 30);
     
+    this->addChild(_scoreLabel, 10);
+    
+    _lastLabel = Label::createWithBMFont("hase_toppo.fnt", "");
+    _lastLabel->setAnchorPoint(Point(0, 0.5));
+    _lastLabel->setScale(0.7);
+    _lastLabel->setPosition(10, visibleSize.height - 30);
+    
+    this->addChild(_lastLabel, 10);
+
     auto listener = EventListenerTouchOneByOne::create();
     
     listener->setSwallowTouches(true);
     listener->onTouchBegan = CC_CALLBACK_2(GameScene::onTouchBegan, this);
+    listener->onTouchMoved = CC_CALLBACK_2(GameScene::onTouchMoved, this);
     listener->onTouchEnded = CC_CALLBACK_2(GameScene::onTouchEnded, this);
     
     this->getEventDispatcher()->addEventListenerWithFixedPriority(listener, 100);
+
+    showScore();
     
     return true;
 }
 
 bool GameScene::onTouchBegan(Touch* touch, Event* event) {
     
-    return true;
-}
-
-void GameScene::onTouchEnded(Touch* touch, Event* event) {
-
     // seed
     srand((unsigned int) time(NULL));
     // 0〜5の間でランダム値を生成して +1 する
     int manId = (rand() % 5) + 1;
-
-    Sprite* man = Sprite::create(StringUtils::format("man_%d.png", manId));
-    man->setScale(1.2);
-    man->setPosition(touch->getLocation().x, 800);
-    PhysicsBody* manBody = PhysicsBody::createBox(man->getBoundingBox().size);
-    man->setPhysicsBody(manBody);
     
-    this->addChild(man, 2);
+    _spawn = Sprite::create(StringUtils::format("man_%d.png", manId));
+    _spawn->setPosition(touch->getLocation().x, POSITION_Y_SPAWN);
+    PhysicsBody* manBody = PhysicsBody::createBox(_spawn->getBoundingBox().size);
+    manBody->setDynamic(false);
+    _spawn->setPhysicsBody(manBody);
+
+    RotateTo* rotate = RotateTo::create(1, 90);
+    RepeatForever* repeat = RepeatForever::create(rotate);
+    _spawn->runAction(repeat);
+    
+    this->addChild(_spawn, 2);
+
+    return true;
+}
+
+void GameScene::onTouchMoved(Touch* touch, Event* event) {
+
+    _spawn->setPosition(touch->getLocation().x, POSITION_Y_SPAWN);
+}
+
+void GameScene::onTouchEnded(Touch* touch, Event* event) {
+
+    _spawn->getActionManager()->removeAllActions();
+    
+    PhysicsBody* manBody = _spawn->getPhysicsBody();
+    manBody->setDynamic(true);
+    manBody->resetForces();
+    
+    _score++;
+    showScore();
+}
+
+void GameScene::showScore() {
+    
+    _scoreLabel->setString(StringUtils::format("スコア:%d", _score));
+
+    std::string last = "";
+    for (int i = 0; i < _last; i++) {
+        last += "●";
+    }
+    _lastLabel->setString(last);
 }
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -10,8 +10,22 @@ public:
     CREATE_FUNC(GameScene);
     
 private:
+    int _score;
+
+    int _last;
+
+    cocos2d::Sprite* _spawn;
+    
+    cocos2d::Label* _scoreLabel;
+
+    cocos2d::Label* _lastLabel;
+
     bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
     
+    void onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* event);
+
     void onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* event);
     
+    void showScore();
+
 };
